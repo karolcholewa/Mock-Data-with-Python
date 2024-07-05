7e9448b4afac1608431b37c08f03fbd5091cac16
Karol Cholewa <karol.ch@gmail.com>
2024-07-05 02:02:21 +0200

    Updated the random email using the ascii_email attribute

--- generateFakerData.py
+++ generateFakerData.py
@@ -9,17 +9,17 @@ custom_domains = ['company1.com', 'company2.net', 'company3.org', 'customdomain.
 
 # Generate mock data
 mock_data = []
-for _ in range(100):
+for _ in range(10):
     domain = fake.random_element(custom_domains)
     mock_data.append({
         'id': fake.unique.random_int(min=100000, max=999999),
-        'email': f"{fake.user_name()}@{domain}",
+        'email': fake.ascii_email(),
         'firstName': fake.first_name(),
-        'lastName': fake.last_name()
+        'lastName': fake.last_name() 
     })
 
 # Convert mock data to a DataFrame
-df = pd.DataFrame(mock_data, columns=['id', 'email', 'firstName', 'lastName'])
+df = pd.DataFrame(mock_data, columns=['id', 'email', 'firstName', 'lastName', 'areaCode'])
 
 # Save to CSV
 df.to_csv('mock_data_table.csv', index=False)
